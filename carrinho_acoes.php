6b48fc21bb18efb544e55b483eb1a9007096e553
camilaberti <camilabertialvesberti@gmail.com>
2019-06-17 17:23:47 -0300

    Primeiro Commit

---
+++ carrinho_acoes.php
@@ -0,0 +1,36 @@
+<?php
+session_start(); //Avisando o PHP que essa página trabalha com sessões.
+
+ /**
+  * Verifico se a variável de ações existe, para então verificar qual ação o usuário ordenou.
+  */
+  if(isset($_GET['acao_carrinho'])){
+        
+      switch($_GET['acao_carrinho']){
+
+         case 'limpar'; //na URL do navegador: carrinho_acoes.php?acao_carrinho-limpar
+             $_SESSION['carrinho'] = array(); //Ao definir o carrinho como array novamente,limpamos ele.
+        break;
+
+        case 'add' ; // adiciona um produto ao carrinho.
+
+        $produto = $_GET();
+        $preco;
+        $qnt;
+        $total = $preco * $qnt;
+
+        $dados_produtos_a_ser_add = array("produto" => $produto, "preco" => $preco, "quantidade" => $qnt,
+                          "total" => $total);
+
+        $_SESSION['carrinho'][] = $dados_produtos_a_ser_add;
+        break;
+        
+        case 'remover';
+          $item_carrinho = $_GET['item'];
+
+          if(isset($_SESSION['carrinho'][$item_carrinho])) { //verificando se o item realmente existe.
+             unset($_SESSION['carrinho'][$item_carrinho]); // se existe, destrói o item.
+            }
+        break;
+      } // fim do switch
+  } // fim do if do isset
